59a0de636bc2c81db6436cde73d64e24e7c26b45
volodymyrrrr <125571431+volodymyrrrr@users.noreply.github.com>
2023-06-14 14:18:01 +0300

    Update form_test.py

--- Shadow_Root/Tests/form_test.py
+++ Shadow_Root/Tests/form_test.py
@@ -12,7 +12,7 @@ from fake_useragent import UserAgent
 
 ua = UserAgent
 
-client = gspread.service_account('Tests/gs_credentials.json')
+client = gspread.service_account('Shadow_Root/Tests/gs_credentials.json')
 working_sheet = client.open_by_url(
     'https://docs.google.com/spreadsheets/d/1fRi9qAdb-E-xAY_jQiMdjjEsN1xZZdxK6865V-Ck6RE/edit#gid=0')
 wb1 = working_sheet.get_worksheet(1)
